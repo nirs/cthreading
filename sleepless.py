a58f2aefe6fcb8bcff1e53aca0f8475b734d03f6
Nir Soffer <nsoffer@redhat.com>
2015-10-19 01:42:05 +0300

    Add sleepless benchmark
    This benchmark tests the cpu usage of waiting for threading.Condition
    with a timeout. Built-in Condition in Python 2 is implemented using
    polling and sleeps, which can use significant cpu time when lots of
    threads are waiting.
    
    cthreading uses the same design as in Python 3, waiting on a blocked
    semaphore using sem_timedwait, which cost no cpu time.
    
    pthreading uses pthread_cond_timedwait(). This introduces undefined
    behavior if the condition is not used properly, and the issue of
    spurious wakeups, which do not exists in sem_timedwait().

---
+++ sleepless.py
@@ -0,0 +1,74 @@
+# Copyright 2015 Nir Soffer <nsoffer@redhat.com>
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions
+# of the GNU General Public License v2 or (at your option) any later version.
+
+import optparse
+import sys
+
+
+def main(args):
+    options, args = parse_args(args)
+
+    if options.monkeypatch:
+        if options.monkeypatch == "cthreading":
+            import cthreading
+            cthreading.monkeypatch()
+        elif options.monkeypatch == "pthreading":
+            import pthreading
+            pthreading.monkey_patch()
+        else:
+            raise ValueError("Usupported monkeypatch %r" % options.monkeypatch)
+
+    try:
+        _range = xrange
+    except NameError:
+        _range = range
+
+    import threading
+
+    if options.profile:
+        import yappi
+        yappi.set_clock_type('cpu')
+        yappi.start(builtins=True, profile_threads=True)
+
+    cond = threading.Condition(threading.Lock())
+    threads = []
+
+    for i in _range(options.threads):
+        t = threading.Thread(target=sleep, args=(cond, options.timeout))
+        t.daemon = True
+        t.start()
+        threads.append(t)
+
+    for t in threads:
+        t.join()
+
+    if options.profile:
+        yappi.stop()
+        stats = yappi.get_func_stats()
+        stats.save(options.profile, 'pstat')
+
+
+def parse_args(args):
+    parser = optparse.OptionParser(usage="sleepless [options]")
+    parser.add_option("-t", "--threads", dest="threads", type="int",
+                      help="number of threads")
+    parser.add_option("-s", "--timeout", dest="timeout", type="int",
+                      help="number of seconds to sleep")
+    parser.add_option("-m", "--monkeypatch", dest="monkeypatch",
+                      help="monkeypatch type (native, cthreading, pthreading)")
+    parser.add_option("-p", "--profile", dest="profile",
+                      help="create profile (requires yappi 0.93)")
+    parser.set_defaults(threads=400, timeout=10, monkeypatch=None)
+    return parser.parse_args(args)
+
+
+def sleep(cond, timeout):
+    with cond:
+        cond.wait(timeout)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
